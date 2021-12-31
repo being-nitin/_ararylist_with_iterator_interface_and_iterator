b79bddb1eedfbd2f7320b24a0b661837ad21563d
Nitin <nsinghboss02@gmail.com>
2021-12-31 18:04:52 +0530

    Initial commit

---
+++ src/com/codechef/Main.java
@@ -0,0 +1,21 @@
+package com.codechef;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Iterator;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Use of iterator interface in arraylist to proceed iteration :
+	 */
+        Scanner sc = new Scanner(System.in);
+    ArrayList <String> s1 = new ArrayList<>(5);
+    for(int i=0;i<3;i++){
+        s1.add(sc.next());
+    }
+    Iterator itr = s1.iterator();
+    while(itr.hasNext()){
+        System.out.println(itr.next());
+    }
+    }
+}
